d235bb8a2b68b726c86c6b3917644e8f09aad0f2
codeinger <codeingercode@gmail.com>
2021-07-31 02:19:27 +0530

    Part-2

--- app/src/main/java/com/codeinger/myapplication/MainActivity.kt
+++ app/src/main/java/com/codeinger/myapplication/MainActivity.kt
@@ -2,28 +2,23 @@ package com.codeinger.myapplication
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
-import android.widget.Button
-import android.widget.Toast
-import kotlinx.coroutines.*
+import android.util.Log
+import kotlinx.coroutines.GlobalScope
+import kotlinx.coroutines.delay
+import kotlinx.coroutines.launch
 
 class MainActivity : AppCompatActivity() {
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val btn : Button = findViewById(R.id.btn);
-        btn.setOnClickListener {
-            CoroutineScope(Dispatchers.IO).launch {
-                doSomeTaskWithCoroutine()
-            }
+        Log.i("fdhjvef", "onCreate : ${Thread.currentThread().name}")
+
+        GlobalScope.launch {
+            Log.i("fdhjvef", "coroutine launch in : ${Thread.currentThread().name}")
+            delay(3000)
+            Log.i("fdhjvef", "delay complete : ${Thread.currentThread().name}")
         }
 
-    }
-
-    private suspend fun doSomeTaskWithCoroutine() {
-        delay(3000)
-        withContext(Dispatchers.Main){
-            Toast.makeText(this@MainActivity,"Task done.",Toast.LENGTH_SHORT).show()
-        }
     }
 }
