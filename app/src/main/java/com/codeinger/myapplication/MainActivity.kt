baf43b605bc3046198f15242303b3c1612b22cf3
codeinger <codeingercode@gmail.com>
2021-08-02 02:26:22 +0530

    Part-6

--- app/src/main/java/com/codeinger/myapplication/MainActivity.kt
+++ app/src/main/java/com/codeinger/myapplication/MainActivity.kt
@@ -1,43 +1,37 @@
 package com.codeinger.myapplication
 
+import android.content.Context
 import android.content.Intent
 import android.os.Bundle
 import android.util.Log
 import android.widget.Button
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
 
 class MainActivity : AppCompatActivity() {
 
+    val TAG = "MyRanDomTag"
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        runBlocking {
-            test1()
+        val text = findViewById<TextView>(R.id.text)
+
+        lifecycleScope.launch(Dispatchers.IO) {
+            val result =  networkCall()
+            withContext(Dispatchers.Main){text.text = result}
         }
 
     }
 
-    suspend fun test(){
-        val job = lifecycleScope.launch {
-            delay(2000)
-            Log.i("fdkjcjkdfn", "2 sec finish: ")
-        }
-
-        job.join()
-        Log.i("fdkjcjkdfn", "test: ")
-    }
-
-    suspend fun test1(){
-        val deferred : Deferred<Int> = lifecycleScope.async {
-            delay(2000)
-            Log.i("fdkjcjkdfn", "2 sec finish: ")
-            10
-        }
-        val value = deferred.await()
-        Log.i("fdkjcjkdfn", "test: "+value)
+    private suspend fun networkCall() : String{
+        Log.i(TAG, "networkCall: start")
+        delay(3000)
+        Log.i(TAG, "networkCall: end")
+        return "Result"
     }
 
 
