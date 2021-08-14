7bc9e395786f151969826d722d941008fc70d9ef
codeinger <codeingercode@gmail.com>
2021-08-15 02:43:52 +0530

    Part-7

--- app/src/main/java/com/codeinger/myapplication/MainActivity.kt
+++ app/src/main/java/com/codeinger/myapplication/MainActivity.kt
@@ -9,6 +9,7 @@ import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import androidx.lifecycle.lifecycleScope
 import kotlinx.coroutines.*
+import kotlin.system.measureTimeMillis
 
 class MainActivity : AppCompatActivity() {
 
@@ -18,20 +19,45 @@ class MainActivity : AppCompatActivity() {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
 
-        val text = findViewById<TextView>(R.id.text)
+        lifecycleScope.launch {
 
-        lifecycleScope.launch(Dispatchers.IO) {
-            val result =  networkCall()
-            withContext(Dispatchers.Main){text.text = result}
+            val time = measureTimeMillis {
+
+                val job = launch {
+                    val s1 = async { networkCall1() }
+                    val s2 = async { networkCall2() }
+                    val s3 = async { networkCall3() }
+                    Log.i(TAG, "onCreate: ${s1.await()}  ${s2.await()} ${s3.await()}")
+                }
+
+                job.join()
+
+                Log.i(TAG, "network call done: ")
+            }
+
+            Log.i(TAG, "onCreate: time $time")
+
         }
+
 
     }
 
-    private suspend fun networkCall() : String{
-        Log.i(TAG, "networkCall: start")
+    suspend fun networkCall1():String{
+        delay(1000)
+        Log.i(TAG, "networkCall1: called")
+        return "result 1"
+    }
+
+    suspend fun networkCall2():String{
         delay(3000)
-        Log.i(TAG, "networkCall: end")
-        return "Result"
+        Log.i(TAG, "networkCall2: called")
+        return "result 2"
+    }
+
+    suspend fun networkCall3():String{
+        delay(2000)
+        Log.i(TAG, "networkCall3: called")
+        return "result 3"
     }
 
 
